dffa9b99ff2158457d43fc30c50d4159840510e3
wlswo <byunjin11@naver.com>
2024-04-06 14:28:38 +0900

    feat : init commit

---
+++ src/main/java/com/lolmoonchul/lolmoonchul/LolmoonchulApplication.java
@@ -0,0 +1,13 @@
+package com.lolmoonchul.lolmoonchul;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LolmoonchulApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LolmoonchulApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lolmoonchul/lolmoonchul/LolmoonchulApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lolmoonchul.lolmoonchul;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LolmoonchulApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
