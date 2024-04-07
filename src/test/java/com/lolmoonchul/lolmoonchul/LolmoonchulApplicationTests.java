4876ac116959a527533c237ca734c42a5b32e500
wlswo <byunjin11@naver.com>
2024-04-07 17:01:53 +0900

    feat : speed develop

--- src/test/java/com/lolmoonchul/lolmoonchul/LolmoonchulApplicationTests.java
+++ src/test/java/com/lolmoonchul/lolmoonchul/LolmoonchulApplicationTests.java
@@ -3,11 +3,11 @@ package com.lolmoonchul.lolmoonchul;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(classes = LolmoonchulApplicationTests.class)
 class LolmoonchulApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
